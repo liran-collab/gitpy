291140deea71e3a475843671658c56f573ec60cb
Liran <astromedagame@gmail.com>
2025-10-19 17:46:55 +0300

    feat(myPy.py): add sort_list function

--- myPy.py
+++ myPy.py
@@ -2,6 +2,10 @@
 list1 : list[int] = [1,3,4,7,9]
 list2 : list[int] = [3,4,8,9,10,12]
 
+def sort_lists(list1 : list[int], list2 : list[int]) -> list[int]:
+    list3 = list1 + list2
+    list3.sort()
+    return (list3)
 
 if __name__ == "__main__":
-    pass
+    print(sort_lists(list1, list2))

c59da005f4c748e935b7f6113652a46a629ba905
Liran <astromedagame@gmail.com>
2025-10-20 10:53:02 +0300

    fix(myPy.py): fix conflict with ending of fucntion

--- myPy.py
+++ myPy.py
@@ -1,7 +1,22 @@
+from google import genai
+
 # Initialize variables
 list1 : list[int] = [1,3,4,7,9]
 list2 : list[int] = [3,4,8,9,10,12]
 
+def ai_calculator(desc):
+    def decorator(func):
+        def wrapper(*args, **kargs):
+            client = genai.Client(api_key="***")
+            response = client.models.generate_content(
+                model="gemini-2.5-flash", contents=desc + " ".join(map(str, args))
+            )
+            print(response.text)
+            return (func(*args, **kargs))
+        return (wrapper)
+    return decorator
+
+@ai_calculator(desc = "print the merge of the following two arrays without any other text: \n")
 def sort_two_list(list1 : list[int], list2: list[int]) -> list[int]:
     list3 : list[int] = []
     i : int  = 0
@@ -14,7 +29,7 @@ def sort_two_list(list1 : list[int], list2: list[int]) -> list[int]:
             list3.append(list2[j])
             j += 1
 
-    print(list3)
+    return (list3)
 
 if __name__ == "__main__":
-    sort_two_list(list1,list2)
+    print(sort_two_list(list1,list2))
